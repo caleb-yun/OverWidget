4df52d70efb02ab5bd86908a8a5eb5acc912d926
Kala30 <caleb.yun@gmail.com>
2017-08-23 16:25:14 -0700

    Fixed network issues

--- app/src/main/java/com/cogentworks/overwidget/RestOperation.java
+++ app/src/main/java/com/cogentworks/overwidget/RestOperation.java
@@ -27,52 +27,52 @@ public class RestOperation extends AsyncTask<String, Void, Profile> {
 
     @Override
     protected Profile doInBackground(String... params) {
-        Profile result = new Profile();
-        String battleTag = new String();
+        Profile result = null;
+        String battleTag = params[0];
         if (battleTag != null) {
-            battleTag = params[0];
-        }
-        try {
-            // Create URL
-            URL endpoint = new URL("https://owapi.net/api/v3/u/" + battleTag.replace('#','-') + "/stats"); //params[0].replace('#','-')
+            result = new Profile();
+            try {
+                // Create URL
+                URL endpoint = new URL("https://owapi.net/api/v3/u/" + battleTag.replace('#', '-') + "/stats"); //params[0].replace('#','-')
 
-            // Create connection
-            HttpsURLConnection urlConnection = (HttpsURLConnection) endpoint.openConnection();
+                // Create connection
+                HttpsURLConnection urlConnection = (HttpsURLConnection) endpoint.openConnection();
 
-            // Set methods and timeouts
-            urlConnection.setRequestMethod("GET");
-            //urlconnection.setReadTimeout(READ_TIMEOUT);
-            //urlconnection.setConnectTimeout(CONNECTION_TIMEOUT);
+                // Set methods and timeouts
+                urlConnection.setRequestMethod("GET");
+                //urlconnection.setReadTimeout(READ_TIMEOUT);
+                //urlconnection.setConnectTimeout(CONNECTION_TIMEOUT);
 
-            // Connect to URL
-            urlConnection.connect();
+                // Connect to URL
+                urlConnection.connect();
 
-            if (urlConnection.getResponseCode() == 200) {
-                // Success
-                InputStream responseBody = new BufferedInputStream(urlConnection.getInputStream());
-                // Parser
-                JsonParser jsonParser = new JsonParser();
-                JsonObject stats = jsonParser.parse(new InputStreamReader(responseBody, "UTF-8"))
-                        .getAsJsonObject().get("us")
-                        .getAsJsonObject().get("stats")
-                        .getAsJsonObject().get("competitive")
-                        .getAsJsonObject().getAsJsonObject("overall_stats");
-                result.SetUser(battleTag, stats.get("avatar").getAsString());
-                Log.d("RestOperation", "SetUser");
-                result.SetLevel(stats.get("level").getAsString(), stats.get("prestige").getAsString(), stats.get("rank_image").getAsString());
-                Log.d("RestOperation", "SetLevel");
-                result.SetRank(stats.get("comprank").getAsString(), stats.get("tier").getAsString());
-                Log.d("RestOperation", "SetRank");
+                if (urlConnection.getResponseCode() == 200) {
+                    // Success
+                    InputStream responseBody = new BufferedInputStream(urlConnection.getInputStream());
+                    // Parser
+                    JsonParser jsonParser = new JsonParser();
+                    JsonObject stats = jsonParser.parse(new InputStreamReader(responseBody, "UTF-8"))
+                            .getAsJsonObject().get("us")
+                            .getAsJsonObject().get("stats")
+                            .getAsJsonObject().get("competitive")
+                            .getAsJsonObject().getAsJsonObject("overall_stats");
+                    result.SetUser(battleTag, stats.get("avatar").getAsString());
+                    Log.d("RestOperation", "SetUser");
+                    result.SetLevel(stats.get("level").getAsString(), stats.get("prestige").getAsString(), stats.get("rank_image").getAsString());
+                    Log.d("RestOperation", "SetLevel");
+                    result.SetRank(stats.get("comprank").getAsString(), stats.get("tier").getAsString());
+                    Log.d("RestOperation", "SetRank");
 
-                responseBody.close();
-                Log.d("RestOperation", "responseBody.close");
+                    responseBody.close();
+                    Log.d("RestOperation", "responseBody.close");
 
-                urlConnection.disconnect();
+                    urlConnection.disconnect();
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
         return result;
     }
--- app/src/main/java/layout/OverWidgetActivity.java
+++ app/src/main/java/layout/OverWidgetActivity.java
@@ -1,11 +1,21 @@
 package layout;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
+import com.cogentworks.overwidget.Profile;
 import com.cogentworks.overwidget.R;
+
+import static android.appwidget.AppWidgetManager.ACTION_APPWIDGET_UPDATE;
 
 /**
  * Implementation of App Widget functionality.
@@ -13,27 +23,44 @@ import com.cogentworks.overwidget.R;
  */
 public class OverWidgetActivity extends AppWidgetProvider {
 
+    private static final String SYNC_CLICKED    = "automaticWidgetSyncButtonClick";
+
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
-        String battleTag = OverWidgetActivityConfigureActivity.loadUserPref(context, appWidgetId).BattleTag;
-        String compRank = OverWidgetActivityConfigureActivity.loadUserPref(context, appWidgetId).CompRank;
-        String tier = OverWidgetActivityConfigureActivity.loadUserPref(context, appWidgetId).Tier;
+        Profile profile = OverWidgetActivityConfigureActivity.loadUserPref(context, appWidgetId);
+        if (profile != null) {
+            String battleTag = profile.BattleTag;
+            String compRank = profile.CompRank;
+            String tier = profile.Tier;
 
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.over_widget_activity);
-        views.setTextViewText(R.id.appwidget_battletag, battleTag);
-        views.setTextViewText(R.id.appwidget_comprank, compRank);
-        //views.setTextViewText(R.id.appwidget_tier, tier);
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+            // Construct the RemoteViews object
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.over_widget_activity);
+            views.setTextViewText(R.id.appwidget_battletag, battleTag);
+            views.setTextViewText(R.id.appwidget_comprank, compRank);
+            views.setTextViewText(R.id.appwidget_tier, tier);
+
+            views.setOnClickPendingIntent(R.id.appwidget_button, getPendingSelfIntent(context, ACTION_APPWIDGET_UPDATE));
+            // Instruct the widget manager to update the widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        Log.d("OverWidgetActivity", "OnUpdate");
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.over_widget_activity);
+
+            // Tell the AppWidgetManager to perform an update on the current App Widget
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
+    }
+
+    protected static PendingIntent getPendingSelfIntent(Context context, String action) {
+        Intent intent = new Intent(context, OverWidgetActivity.class);
+        intent.setAction(action);
+        return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 
     @Override
@@ -52,6 +79,27 @@ public class OverWidgetActivity extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        Log.d("OverWidgetActivity", "OnReceive");
+
+        if (SYNC_CLICKED.equals(intent.getAction())) {
+            Log.d("OverWidgetActivity", "Refreshed");
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+
+            RemoteViews remoteViews;
+            ComponentName watchWidget;
+
+            remoteViews = new RemoteViews(context.getPackageName(), R.layout.over_widget_activity);
+            watchWidget = new ComponentName(context, OverWidgetActivity.class);
+
+            //remoteViews.setTextViewText(R.id.appwidget_button, "TESTING");
+
+            appWidgetManager.updateAppWidget(watchWidget, remoteViews);
+        }
     }
 }
 
