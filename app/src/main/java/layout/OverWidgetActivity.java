32768e1fe28133c6b168789076f4dfa446987e05
Kala30 <caleb.yun@gmail.com>
2017-08-23 17:09:44 -0700

    Added tap to refresh

--- app/src/main/java/layout/OverWidgetActivity.java
+++ app/src/main/java/layout/OverWidgetActivity.java
@@ -25,8 +25,7 @@ public class OverWidgetActivity extends AppWidgetProvider {
 
     private static final String SYNC_CLICKED    = "automaticWidgetSyncButtonClick";
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
         Profile profile = OverWidgetActivityConfigureActivity.loadUserPref(context, appWidgetId);
         if (profile != null) {
             String battleTag = profile.BattleTag;
@@ -39,7 +38,7 @@ public class OverWidgetActivity extends AppWidgetProvider {
             views.setTextViewText(R.id.appwidget_comprank, compRank);
             views.setTextViewText(R.id.appwidget_tier, tier);
 
-            views.setOnClickPendingIntent(R.id.appwidget_button, getPendingSelfIntent(context, ACTION_APPWIDGET_UPDATE));
+            views.setOnClickPendingIntent(R.id.appwidget_layout, getPendingSelfIntent(context, SYNC_CLICKED, appWidgetId));
             // Instruct the widget manager to update the widget
             appWidgetManager.updateAppWidget(appWidgetId, views);
         }
@@ -50,16 +49,15 @@ public class OverWidgetActivity extends AppWidgetProvider {
         Log.d("OverWidgetActivity", "OnUpdate");
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.over_widget_activity);
-
             // Tell the AppWidgetManager to perform an update on the current App Widget
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
     }
 
-    protected static PendingIntent getPendingSelfIntent(Context context, String action) {
+    protected static PendingIntent getPendingSelfIntent(Context context, String action, int appWidgetId) {
         Intent intent = new Intent(context, OverWidgetActivity.class);
         intent.setAction(action);
+        intent.putExtra("WIDGET_ID", appWidgetId);
         return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 
@@ -91,14 +89,18 @@ public class OverWidgetActivity extends AppWidgetProvider {
             AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
 
             RemoteViews remoteViews;
-            ComponentName watchWidget;
+            ComponentName overWidget;
 
             remoteViews = new RemoteViews(context.getPackageName(), R.layout.over_widget_activity);
-            watchWidget = new ComponentName(context, OverWidgetActivity.class);
+            overWidget = new ComponentName(context, OverWidgetActivity.class);
 
-            //remoteViews.setTextViewText(R.id.appwidget_button, "TESTING");
+            Bundle extras = intent.getExtras();
 
-            appWidgetManager.updateAppWidget(watchWidget, remoteViews);
+            if (extras != null) {
+                int appWidgetId = (int) extras.get("WIDGET_ID");
+                this.updateAppWidget(context, appWidgetManager, appWidgetId);
+                Toast.makeText(context, "Refreshed", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
