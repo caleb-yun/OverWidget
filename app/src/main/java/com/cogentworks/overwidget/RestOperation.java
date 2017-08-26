177d7d6a051f9392a921302d254d6545492395a6
Kala30 <caleb.yun@gmail.com>
2017-08-25 21:37:18 -0700

    Added support for all platforms and regions

--- app/src/main/java/com/cogentworks/overwidget/RestOperation.java
+++ app/src/main/java/com/cogentworks/overwidget/RestOperation.java
@@ -28,50 +28,66 @@ public class RestOperation extends AsyncTask<String, Void, Profile> {
     @Override
     protected Profile doInBackground(String... params) {
         Profile result = null;
+
         String battleTag = params[0];
-        if (battleTag != null) {
-            result = new Profile();
-            try {
-                // Create URL
-                URL endpoint = new URL("https://owapi.net/api/v3/u/" + battleTag.replace('#', '-') + "/stats"); //params[0].replace('#','-')
+        String platform = params[1];
+        String region;
+        if (platform != null) {
+            if (platform.equals("PC")) {
+                region = params[2]; // Set region on PC
+            } else {
+                region = "any"; // Set region to any on console
+            }
 
-                // Create connection
-                HttpsURLConnection urlConnection = (HttpsURLConnection) endpoint.openConnection();
+            if (battleTag != null) {
+                result = new Profile();
+                try {
+                    // Create URL
+                    URL endpoint = new URL("https://owapi.net/api/v3/u/" + battleTag.replace('#', '-') + "/stats?platform=" + platform.toLowerCase());
 
-                // Set methods and timeouts
-                urlConnection.setRequestMethod("GET");
-                //urlconnection.setReadTimeout(READ_TIMEOUT);
-                //urlconnection.setConnectTimeout(CONNECTION_TIMEOUT);
+                    // Create connection
+                    HttpsURLConnection urlConnection = (HttpsURLConnection) endpoint.openConnection();
 
-                // Connect to URL
-                urlConnection.connect();
+                    // Set methods and timeouts
+                    urlConnection.setRequestMethod("GET");
+                    //urlconnection.setReadTimeout(READ_TIMEOUT);
+                    //urlconnection.setConnectTimeout(CONNECTION_TIMEOUT);
 
-                if (urlConnection.getResponseCode() == 200) {
-                    // Success
-                    InputStream responseBody = new BufferedInputStream(urlConnection.getInputStream());
-                    // Parser
-                    JsonParser jsonParser = new JsonParser();
-                    JsonObject stats = jsonParser.parse(new InputStreamReader(responseBody, "UTF-8"))
-                            .getAsJsonObject().get("us")
-                            .getAsJsonObject().get("stats")
-                            .getAsJsonObject().get("competitive")
-                            .getAsJsonObject().getAsJsonObject("overall_stats");
-                    result.SetUser(battleTag, stats.get("avatar").getAsString());
-                    Log.d("RestOperation", "SetUser");
-                    result.SetLevel(stats.get("level").getAsString(), stats.get("prestige").getAsString(), stats.get("rank_image").getAsString());
-                    Log.d("RestOperation", "SetLevel");
-                    result.SetRank(stats.get("comprank").getAsString(), stats.get("tier").getAsString());
-                    Log.d("RestOperation", "SetRank");
+                    // Connect to URL
+                    urlConnection.connect();
 
-                    responseBody.close();
-                    Log.d("RestOperation", "responseBody.close");
+                    if (urlConnection.getResponseCode() == 200) {
+                        // Success
+                        InputStream responseBody = new BufferedInputStream(urlConnection.getInputStream());
+                        // Parser
+                        JsonParser jsonParser = new JsonParser();
+                        JsonObject stats = jsonParser.parse(new InputStreamReader(responseBody, "UTF-8"))
+                                .getAsJsonObject().get(region.toLowerCase()) // Select Region
+                                .getAsJsonObject().get("stats")
+                                .getAsJsonObject().get("competitive")
+                                .getAsJsonObject().getAsJsonObject("overall_stats");
+                        result.SetUser(battleTag, stats.get("avatar").getAsString());
+                        Log.d("RestOperation", "SetUser");
+                        result.SetLevel(stats.get("level").getAsString(), stats.get("prestige").getAsString(), stats.get("rank_image").getAsString());
+                        Log.d("RestOperation", "SetLevel");
+                        result.SetRank(stats.get("comprank").getAsString(), stats.get("tier").getAsString());
+                        Log.d("RestOperation", "SetRank");
 
+                        responseBody.close();
+                        Log.d("RestOperation", "responseBody.close");
+                    } else {
+                        // Error
+                        Log.e("RestOperation", urlConnection.getResponseMessage());
+                        result = null;
+                    }
                     urlConnection.disconnect();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                    result = null;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    result = null;
                 }
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
             }
         }
         return result;
--- app/src/main/java/layout/OverWidgetActivityConfigureActivity.java
+++ app/src/main/java/layout/OverWidgetActivityConfigureActivity.java
@@ -1,15 +1,17 @@
 package layout;
 
-import android.app.Activity;
 import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.cogentworks.overwidget.Profile;
 import com.cogentworks.overwidget.R;
@@ -24,26 +26,57 @@ import java.util.concurrent.ExecutionException;
 public class OverWidgetActivityConfigureActivity extends AppCompatActivity {
 
     private static final String PREFS_NAME = "layout.OverWidgetActivity";
-    private static final String PREF_PREFIX_KEY = "appwidget_";
+    private static final String PREF_PREFIX_KEY = "overwidget_";
     int mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
+
     EditText mAppWidgetText;
+    ProgressBar progressBar;
+    Spinner platformSpinner;
+    Spinner regionSpinner;
+
     View.OnClickListener mOnClickListener = new View.OnClickListener() {
         public void onClick(View v) {
+            progressBar.setVisibility(View.VISIBLE);
+
             final Context context = OverWidgetActivityConfigureActivity.this;
 
             // When the button is clicked, store the string locally
-            String widgetText = mAppWidgetText.getText().toString();
-            saveTitlePref(context, mAppWidgetId, widgetText);
+            String battleTag = mAppWidgetText.getText().toString();
+            String platform = platformSpinner.getSelectedItem().toString();
+            String region = regionSpinner.getSelectedItem().toString();
+            savePrefs(context, mAppWidgetId, battleTag, platform, region);
 
-            // It is the responsibility of the configuration activity to update the app widget
-            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-            OverWidgetActivity.updateAppWidget(context, appWidgetManager, mAppWidgetId);
+            // Check if user exists
+            Profile result = new Profile();
+            try {
+                RestOperation restOperation = new RestOperation();
+                result = restOperation.execute(battleTag, platform, region).get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                result = null;
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+                result = null;
+            }
 
-            // Make sure we pass back the original appWidgetId
-            Intent resultValue = new Intent();
-            resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
-            setResult(RESULT_OK, resultValue);
-            finish();
+            // Profile Exists
+            if (result != null) {
+                // It is the responsibility of the configuration activity to update the app widget
+                AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+                OverWidgetActivity.updateAppWidget(context, appWidgetManager, mAppWidgetId);
+
+                // Make sure we pass back the original appWidgetId
+                Intent resultValue = new Intent();
+                resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
+                setResult(RESULT_OK, resultValue);
+                finish();
+            } else if (result == null) {
+                Toast.makeText(context, "Player not found", Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.GONE);
+            } else {
+                Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.GONE);
+            }
         }
     };
 
@@ -52,9 +85,11 @@ public class OverWidgetActivityConfigureActivity extends AppCompatActivity {
     }
 
     // Write the prefix to the SharedPreferences object for this widget
-    static void saveTitlePref(Context context, int appWidgetId, String text) {
+    static void savePrefs(Context context, int appWidgetId, String battleTag, String platform, String region) {
         SharedPreferences.Editor prefs = context.getSharedPreferences(PREFS_NAME, 0).edit();
-        prefs.putString(PREF_PREFIX_KEY + appWidgetId, text);
+        prefs.putString(PREF_PREFIX_KEY + appWidgetId + "_battletag", battleTag);
+        prefs.putString(PREF_PREFIX_KEY + appWidgetId + "_platform", platform);
+        prefs.putString(PREF_PREFIX_KEY + appWidgetId + "_region", region);
         prefs.apply();
     }
 
@@ -62,11 +97,13 @@ public class OverWidgetActivityConfigureActivity extends AppCompatActivity {
     // If there is no preference saved, get the default from a resource
     static Profile loadUserPref(Context context, int appWidgetId) {
         SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, 0);
-        String battleTag = prefs.getString(PREF_PREFIX_KEY + appWidgetId, null);
+        String battleTag = prefs.getString(PREF_PREFIX_KEY + appWidgetId + "_battletag", null);
+        String platform = prefs.getString(PREF_PREFIX_KEY + appWidgetId + "_platform", null);
+        String region = prefs.getString(PREF_PREFIX_KEY + appWidgetId + "_region", null);
         Profile result = new Profile();
         try {
             RestOperation restOperation = new RestOperation();
-            result = restOperation.execute(battleTag).get();
+            result = restOperation.execute(battleTag, platform, region).get();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
@@ -90,8 +127,25 @@ public class OverWidgetActivityConfigureActivity extends AppCompatActivity {
         setResult(RESULT_CANCELED);
 
         setContentView(R.layout.over_widget_activity_configure);
+
+        progressBar = (ProgressBar)findViewById(R.id.progress_bar);
+        progressBar.setVisibility(View.GONE);
+
         mAppWidgetText = (EditText) findViewById(R.id.appwidget_text);
         findViewById(R.id.add_button).setOnClickListener(mOnClickListener);
+
+        platformSpinner = (Spinner) findViewById(R.id.spinner_platform);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> platformAdapter = ArrayAdapter.createFromResource(this, R.array.platform_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        platformAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        platformSpinner.setAdapter(platformAdapter);
+
+        regionSpinner = (Spinner) findViewById(R.id.spinner_region);
+        ArrayAdapter<CharSequence> regionAdapter = ArrayAdapter.createFromResource(this, R.array.region_array, android.R.layout.simple_spinner_item);
+        regionAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        regionSpinner.setAdapter(regionAdapter);
 
         // Find the widget id from the intent.
         Intent intent = getIntent();
